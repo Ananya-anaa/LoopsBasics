527602e7acdd89146d1521127d4d1d91b78cb521
Ananya-anaa <ananyanayak2807@gmail.com>
2024-05-22 18:00:37 +0530

    Operator

---
+++ Operators.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLoops
+{
+    internal class Operators
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            string s = "hy";
+            string s1 = "hi";
+            char c = 'a';
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine("hello" + s);
+            Console.WriteLine(c + s1);
+            Console.WriteLine(a++);
+            Console.WriteLine(a % b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(a < b);
+            Console.WriteLine(a > b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a <= b);
+        }
+    }
+}
