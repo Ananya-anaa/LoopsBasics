f591066c0ccf90d75eb8a29544fa9972a6643665
Ananya-anaa <ananyanayak2807@gmail.com>
2024-05-22 15:33:45 +0530

    Added : loops code

---
+++ Loops.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppLoops
+{
+    internal class Loops
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
