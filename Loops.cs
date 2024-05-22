66233b3ad6de71298a50e46c7467f834480ef26a
Ananya-anaa <ananyanayak2807@gmail.com>
2024-05-22 18:00:52 +0530

    Operator

--- Loops.cs
+++ Loops.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ConsoleAppLoops
-{
-    internal class Loops
-    {
-        static void Main(string[] args)
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(i);
-            }
-        }
-    }
-}
+//namespace ConsoleAppLoops
+//{
+//    internal class Loops
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine(i);
+//            }
+//        }
+//    }
+//}
